06dea870cc238590f8a193a28f7bd1a08e7ab832
Stellah25 <stellahademba@gmail.com>
2025-03-21 17:37:42 +0300

    Add files via upload

---
+++ Stellah.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub CheckBox5_CheckedChanged(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
